8af9a9b213e90bc1fe484dcbeabca89442cf917d
nickwins <nickwins@protonmail.com>
2016-03-04 11:30:15 -0500

    Add comments

--- Chapter_1/Exercise_1_1/Exercise_1_1.java
+++ Chapter_1/Exercise_1_1/Exercise_1_1.java
@@ -1,7 +1,13 @@
+/*
+* Exercise 1.1 from Liang v10
+* Author: Nick W.
+* Date: 04 Mar 2016
+*/
+
 public class Exercise_1_1 {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Java");
-		System.out.println("Welcome to Computer Science");
-		System.out.println("Programming is fun");
+		System.out.println("Welcome to Java"); // print line 1
+		System.out.println("Welcome to Computer Science"); // print line 2
+		System.out.println("Programming is fun"); // print line 3
 	}
 }
--- Chapter_1/Exercise_1_3/Exercise_1_3.java
+++ Chapter_1/Exercise_1_3/Exercise_1_3.java
@@ -1,5 +1,12 @@
+/*
+* Exercise 1.3 from Liang v10
+* Author: Nick W.
+* Date: 04 Mar 2016
+*/
+
 public class Exercise_1_3 {
 	public static void main(String[] args) {
+		// draw in the letters just like any other ascii art
 		System.out.println("   J     A     V     V     A");
 		System.out.println("   J    A A     V   V     A A");
 		System.out.println("J  J   AAAAA     V V     AAAAA");
