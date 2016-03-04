247526db11405a252db57f21e2c1f10f41ed341e
nickwins <nickwins@protonmail.com>
2016-03-04 11:36:30 -0500

    Add Ex 1.5

---
+++ Chapter_1/Exercise_1_5/Exercise_1_5.java
@@ -0,0 +1,13 @@
+/*
+* Exercise 1.5 from Liang v10
+* Author: Nick W.
+* Date: 04 Mar 2016
+*/
+
+public class Exercise_1_5 {
+	public static void main(String[] args) {
+		// output calculation using parens
+		// to ensure proper order of operations
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+	}
+}
